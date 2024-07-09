e2418c39253bfa0215f43070b1f018ebd3ca3e93
Glen Huang <me@glenhuang.com>
2024-07-09 18:25:14 +0800

    Initial commit

---
+++ src/Red.svelte
@@ -0,0 +1,18 @@
+<script>
+    function show(node) {
+        return {
+            duration: 500,
+            css: (t) => `opacity: ${t}`,
+        }
+    }
+</script>
+
+<div in:show></div>
+
+<style>
+    div {
+        width: 100px;
+        height: 100px;
+        background: red;
+    }
+</style>
